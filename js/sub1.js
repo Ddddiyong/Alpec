b6c0bdfa66b09d61fb7375923e9f9674dca9b3c6
csslick <digitone@naver.com>
2020-05-25 11:06:15 +0900

    sub1 slide down 수정

---
+++ js/sub1.js
@@ -0,0 +1,63 @@
+
+$(function () {
+    $('.vis-depth3 > li').click(function () {
+        // 클래스 제거(상태 초기화)
+        $('.vis-depth3 > li').removeClass('selected');
+
+        // 클래스 추가
+        $(this).addClass('selected');
+
+        // a태그 하이퍼링크 기본 이벤트 해제
+        return false;
+    });
+
+
+
+    $('.depth2').hide();
+    $('.vis-depth2').hide();
+
+    $('.mid_depth > li').hover(
+        function () {
+            $('.depth2').stop().show();
+            $('#header_nav').css('background', '#fff');
+            $('.mid_depth > li > a').css('color', 'black');
+            $('.right_depth > li > a').css('color', 'black');
+            $('.right_depth > li').css('border', '1px solid black');
+            $('.right_depth > li').css('border-radius', '20px');
+            $('.right_depth > li:first').css('border', 'none');
+            $('.btn_Ham').css('color', 'black');
+
+        },
+        function () {
+            $('.depth2').stop().hide();
+            $('#header_nav').css('background', 'none');
+            $('.mid_depth > li > a').css('color', '#000');
+            $('.right_depth > li').css('border', '1px solid white');
+            $('.right_depth > li > a').css('color', 'white');
+            $('.right_depth > li').css('border-radius', '20px');
+            $('.right_depth > li:first').css('border', 'none');
+        }
+    );
+    $('.vis-depth1 > li').click(function () {
+        $('.vis-depth2').stop().slideUp();
+        $(this).children('.vis-depth2').stop().slideToggle();
+    });
+    $('.vis-depth2').mouseout(function () {
+        $(this).children('.vis-depth2').stop().slideUp();
+        console.log('out')
+    });
+})
+
+
+$(document).ready(function () {
+    $(".rightImg").unbind();
+    $(".rightImg").bind("click", function () {
+        $(".sliderInbox").stop();
+        $(".sliderInbox").animate({ left: -980 }, 500);
+    });
+    $(".leftImg").unbind();
+    $(".leftImg").bind("click", function () {
+        $(".sliderInbox").stop();
+        $(".sliderInbox").animate({ left: 0 }, 500);
+    });
+});
